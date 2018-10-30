0ec7f980086bde2610bb53acf2b4684a8f0eafe7
srbbr <44610092+srbbr@users.noreply.github.com>
2018-10-30 14:36:26 +0100

    Add files via upload

---
+++ verifyCredentials.js
@@ -0,0 +1,17 @@
+﻿const DMSAPI = require('./lib/dmsapi.js');
+
+async function verify(cfg, cb) {
+    console.log('about to verify credentials');
+    const oDMS = new DMSAPI();
+    const oRes = await oDMS.Login(DMSAPI.TrimURL(cfg.dms_url) + '/dms', cfg.dms_user, cfg.dms_pass);
+    if (oRes.result) {
+        await oDMS.Logout();
+        console.log('verification successful');
+        cb(null, { verified: true });
+    } else {
+        console.log('verification failed: ' + oRes.message);
+        cb(new Error(oRes.message), { verified: false });
+    }
+};
+
+module.exports = verify;
